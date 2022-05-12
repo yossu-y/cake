dccba4f0acff7666a4c4545a5d44b76af52ba811
yossu-y <yangyuub@gmail.com>
2022-05-12 13:08:48 +0000

    [update]views/public/registrations/new

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  root to: "homes#top"
+  get "homes/about" => "homes#about",as: "about"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20220512115411_devise_create_customers.rb
+++ db/migrate/20220512115411_devise_create_customers.rb
@@ -31,7 +31,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+post
       t.string :last_name, null: false
       t.string :first_name, null: false
       t.string :last_name_kana, null: false
