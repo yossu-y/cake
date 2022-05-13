b4c851d46850d34e4d83a12e4faf15a94647590c
yossu-y <yangyuub@gmail.com>
2022-05-13 02:28:55 +0000

    [update]退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +18,16 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_valid
+        redirect_to new_user_session_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
 
-  root to: "homes#top"
-  get "homes/about" => "homes#about",as: "about"
+  scope :public do
+    root to: "homes#top"
+    get "homes/about" => "homes#about",as: "about"
+  end
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
